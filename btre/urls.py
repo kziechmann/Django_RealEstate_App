6fcbe669b616b56a6f49979cd04b6690feb9465f
Kevin <kziechmann@gmail.com>
2019-01-04 14:39:03 -0700

    Used django-admin to create basic app with home page and about.

--- btre/urls.py
+++ btre/urls.py
@@ -1,12 +1,7 @@
+from django.conf.urls import url , include
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('pages.urls')),
-    path('listings/', include('listings.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('contacts/', include('contacts.urls')),
-    path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url('', include('pages.urls')),
+    url(r'^admin/', admin.site.urls),
+]
--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('about', views.about, name='about'),
+urlpatterns =[
+    path('', views.index,  name='index'),
+    path('about', views.about,  name='about')
 ]
